53243b7fd4c7b3ed4ee61353a8c7a2122b5d83dc
Amul M <112810764+programmeramul@users.noreply.github.com>
2026-03-21 21:12:02 +0530

    Delete DAY 6.sql

--- DAY 6.sql
+++
@@ -1,8 +0,0 @@
-Create table users (
-id INT auto_increment Primary Key,
-name VARCHAR (100) NOT NULL,
-email VARCHAR (100) NOT NULL,
-gender ENUM ('Male','Female','Other'),
-date_of_birth DATE,
-created_at timestamp default current_timestamp
-);
